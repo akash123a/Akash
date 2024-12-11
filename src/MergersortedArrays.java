1b12c53b4c5e9af9bc4e2999dc6b4fd9f41d8207
Akash Gupta <akashgupta26121999@gmail.com>
2024-12-11 10:45:10 +0530

    Your commit message next

--- src/MergersortedArrays.java
+++ src/MergersortedArrays.java
@@ -1,7 +1,23 @@
 import  java.util.Arrays;
 
 public class MergersortedArrays {
-
+//    Examples:
+//
+//    Input: a[] = [2, 4, 7, 10], b[] = [2, 3]
+//    Output:
+//            2 2 3 4
+//            7 10
+//    Explanation: After merging the two non-decreasing arrays, we get, 2 2 3 4 7 10
+//    Input: a[] = [1, 5, 9, 10, 15, 20], b[] = [2, 3, 8, 13]
+//    Output:
+//            1 2 3 5 8 9
+//            10 13 15 20
+//    Explanation: After merging two sorted arrays we get 5 10 12 18 20.
+//    Input: a[] = [0, 1], b[] = [2, 3]
+//    Output:
+//            0 1
+//            2 3
+//    Explanation: After merging two sorted arrays we get 0 1 2 3.
 
     public static void mergetoString(int[] arr1, int arr2[]) {
         int n = arr1.length;
